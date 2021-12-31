c16dad5ec2e6c271057c0cf36c1164ffc0120958
sfumecjf <2410913218@qq.com>
2021-12-31 20:27:10 +0800

    successful qtGUI with cmake

--- main.cpp
+++ main.cpp
@@ -7,9 +7,19 @@
 #include <QTime>
 
 using namespace std;
+//using namespace cv;
 
 int main(int argc, char *argv[])
 {
    cout << "Hello Qt-CMake!" << endl;
-   return 0;
+
+   QApplication a(argc, argv);
+
+   MainWindow mainWin;
+   mainWin.show();
+
+//   Mat image = Mat.ones(20, 20);
+//   imshow("black",image);
+
+   return a.exec();
 }
