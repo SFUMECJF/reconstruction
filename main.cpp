4bca984621c2d29dbeb425db61899c238bbc31ba
sfumecjf <2410913218@qq.com>
2021-12-31 20:08:58 +0800

    first cmake qt program

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "mainwindow.h"
+#include <QApplication>
+#include <QWidget>
+#include <QDebug>
+#include <thread>
+#include <QTime>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+   cout << "Hello Qt-CMake!" << endl;
+   return 0;
+}
